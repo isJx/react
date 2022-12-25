a5673cb342d1239ecb7c02d814acd1be6eaa3a31
jinxu <J634167397@163.com>
2022-12-25 13:22:52 +0800

    chore: 修改项目逻辑

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,15 @@
+import { Provider } from "react-redux";
+import { RouterProvider } from "react-router-dom";
 import "./App.css";
+import router from "./router";
+import store from "./store/store";
 
 export default function App() {
-  return <div className="App">App</div>;
+  return (
+    <div className="App">
+      <Provider store={store}>
+        <RouterProvider router={router} />
+      </Provider>
+    </div>
+  );
 }
--- src/main.tsx
+++ src/main.tsx
@@ -1,18 +1,12 @@
 import ReactDOM from "react-dom/client";
 import "./index.css";
 
-import "@arco-design/web-react/dist/css/arco.css";
+import App from "./App";
 
-import router from "@/router";
-import { RouterProvider } from "react-router-dom";
+import "@arco-design/web-react/dist/css/arco.css";
 
 import "uno.css";
 
-import { Provider } from "react-redux";
-import store from "./store/store";
-
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
-  <Provider store={store}>
-    <RouterProvider router={router} />
-  </Provider>
+  <App />
 );
