75746a4cce69aec8e03f1f0fef246c85420ab45b
jinxu <J634167397@163.com>
2022-12-25 17:54:13 +0800

    delete: delete App page

--- src/main.tsx
+++ src/main.tsx
@@ -1,12 +1,16 @@
 import ReactDOM from "react-dom/client";
 import "./index.css";
 
-import App from "./App";
-
 import "@arco-design/web-react/dist/css/arco.css";
 
+import { Provider } from "react-redux";
+import { RouterProvider } from "react-router-dom";
 import "uno.css";
+import router from "./router";
+import store from "./store/store";
 
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
-  <App />
+  <Provider store={store}>
+    <RouterProvider router={router} />
+  </Provider>
 );
